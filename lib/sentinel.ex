49d96aaa5526f31d6b646ef2db40842035099bdb
William Huba <whuba@spotify.com>
2019-05-01 14:33:01 -0400

    Add functions to create installation-specific clients.

--- lib/sentinel.ex
+++ lib/sentinel.ex
@@ -2,7 +2,15 @@ defmodule Sentinel do
   @doc """
   Generate a GitHub client authenticated with a JWT.
   """
-  def client do
+  def client() do
     Tentacat.Client.new(%{jwt: Sentinel.Auth.token()})
+  end
+
+  @doc """
+  Generate a GitHub client for a specific installation
+  """
+  def client(installation) when is_integer(installation) do
+    token = Sentinel.Installations.token(installation)
+    Tentacat.Client.new(%{access_token: token})
   end
 end
---
+++ lib/sentinel/installations.ex
@@ -0,0 +1,36 @@
+defmodule Sentinel.Installations do
+  @moduledoc """
+  Handles installations of the GitHub app. Each installation requires its own access token to access, and could consist
+  of zero to many repositories within a given organization.
+  """
+
+  defp response_body({code, body, _httpoison_response}) when code >= 200 and code < 300, do: body
+  defp response_body(response), do: {:error, response}
+
+  @doc """
+  List all installations for this application.
+  """
+  def list() do
+    Sentinel.client()
+    |> Tentacat.App.Installations.list_mine()
+    |> response_body()
+  end
+
+  def list_ids() do
+    list()
+    |> Enum.map(fn %{"id" => id} -> id end)
+  end
+
+  @doc """
+  Get an access token for a given installation.
+  """
+  def token(%{"id" => id}), do: token(id)
+  def token(installation) when is_integer(installation) do
+    Sentinel.client()
+    |> Tentacat.App.Installations.token(installation)
+    |> response_body()
+    |> Map.get("token")
+  end
+
+
+end
